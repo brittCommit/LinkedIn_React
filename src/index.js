97cfd2562f44756e319b8d6ca29bc5ebdce6fc6c
Brittney Huttner <brittneyhuttner@gmail.com>
2020-09-16 20:24:56 -0700

    using jsx & objects

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+let city = {
+  name: "Tulum",
+  country: "Mexico"
+};
+
 ReactDOM.render(
-  React.createElement("h1", {style: {color: "green"}}, "Hello!"),
+  <h1>
+    {city.name} is in {city.country}
+  </h1>,
   document.getElementById('root')
 );
 
