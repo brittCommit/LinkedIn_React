cbea1644e757c2bec51b413980c185d1d70c5cb2
Brittney Huttner <brittneyhuttner@gmail.com>
2020-09-16 21:12:02 -0700

    rendered list of objects

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Lake({name}) {
-    return <h1>{name}</h1>;
-}
+const lakeList = [
+                { id: "1", name: "Echo", trailhead: "Echo"},
+                { id: "2", name: "Maud", trailhead: "Wrights"},
+                { id: "3", name: "Velma", trailhead: "Bayview"} 
+];
 
-function App() {
+function App({lakes}) {
   return (
-  <div>
-    <Lake name = "Lake Tahoe" />
-    <Lake name = "Angora Lake" />
-    <Lake name = "Boca" />
-  </div>
+    <div>
+      {lakes.map(lake => (
+        <div>
+          <h2>{lake.name}</h2>
+          <p>Accessed by: {lake.trailhead}</p>
+        </div>
+      ))}
+    </div>
   );
 }
 
