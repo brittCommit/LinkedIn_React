a7d61dce602edc43eb82c9ec063339abdd10904c
Brittney Huttner <brittneyhuttner@gmail.com>
2020-09-17 09:29:51 -0700

    changing status using useState hooks

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Lake() {
-  return <h1>Lake!</h1>;
-}
 
-function SkiResort() {
-  return <h1>Ski Resort</h1>;
-}
-
-function App ({name}) {
+function App () {
+  const [status, setStatus] = useState("Open");
   return (
-    <>
-      <h1>{name}</h1>
-      <Lake/>
-      <SkiResort/>
-    </>
+   <div>
+     <h1>Status: {status}</h1>
+     <button onClick={() => setStatus("Closed")}>Closed</button>
+     <button onClick={() => setStatus("Open")}>Open</button>
+     <button onClick={() => setStatus("Back in 5")}>Break</button>
+   </div>
   );
 }
 
 ReactDOM.render(
-  <App name= "React"/>,
+  <App />,
   document.getElementById('root')
 );
 
